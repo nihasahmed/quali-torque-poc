bb1ab8d484430b16bdc43772089615bac95b2202
nihasahmed <46564793+nihasahmed@users.noreply.github.com>
2024-08-02 18:24:06 +0530

    changed region

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = "YOUR_PROJECT_ID"
-  region      = "europe-west1"
-  zone        = "europe-west1-c"
+  region      = "us-central1"
+  zone        = "us-central1-c"
 }
 
 resource "google_compute_instance" "default" {
