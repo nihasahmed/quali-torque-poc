50dba98e702158f60709d2a9d1d5f3f22aae29c6
nihasahmed <46564793+nihasahmed@users.noreply.github.com>
2024-08-05 21:21:56 +0530

    Added variables.tf

---
+++ vm-custom/variables.tf
@@ -0,0 +1,41 @@
+variable "project_id" {
+  type        = string
+  description = "The GCP project ID"
+  default     = "dev-byos"
+}
+
+variable "project_region" {
+  type        = string
+  description = "Region of project"
+  default     = "us-central1"
+}
+
+variable "vm_name" {
+  type        = string
+  description = "Name of VM"
+}
+
+variable "vm_type" {
+  type        = string
+  description = "Name of VM"
+  default = "e2-micro"
+}
+
+variable "vm_zone" {
+  type        = string
+  description = "Region of project"
+  default     = "us-central1"
+}
+
+variable "vm_disk_space" {
+  type        = number
+  description = "Size of disk added to VM"
+  default     = "20"
+}
+
+variable "vm_image_name" {
+  type        = string
+  description = "Name of VM image"
+  default = "ubuntu-os-cloud/ubuntu-2004-focal-v20220712"
+}
+
